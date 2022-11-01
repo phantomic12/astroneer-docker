9d253dc1f1f96159b86b8839caeb46345a29e14b
Paolo Falomo <paolofalomo@gmail.com>
2022-11-01 02:42:06 +0100

    changed port

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ crudini --set /astroneer/Astro/Saved/Config/WindowsServer/AstroServerSettings.in
 crudini --set /astroneer/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini "/Script/Astro.AstroServerSettings" "OwnerName" $OWNER_NAME
 crudini --set /astroneer/Astro/Saved/Config/WindowsServer/AstroServerSettings.ini "/Script/Astro.AstroServerSettings" "OwnerGuid" $OWNER_GUID
 
-crudini --set /astroneer/Astro/Saved/Config/WindowsServer/Engine.ini "url" "Port" 8777
+crudini --set /astroneer/Astro/Saved/Config/WindowsServer/Engine.ini "url" "Port" 30877
 crudini --set /astroneer/Astro/Saved/Config/WindowsServer/Engine.ini "SystemSettings" "net.AllowEncryption" False
 
 /var/lib/flatpak/runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable/active/files/proton run /astroneer/AstroServer.exe PROTON_LOG=! %command%
